d1b0738677a7fcd21bb3be59a4f8ececf0e95d1d
Kyle Hiroyasu <kyle.hiroyasu@valiton.com>
2020-04-06 20:14:02 +0200

    updated models for group e, tried to make naming consistent with group d

--- backend-zs/models/user.model.js
+++ backend-zs/models/user.model.js
@@ -1,15 +1,48 @@
-const mongoose = require("mongoose");
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+var passport = require('passport');
 
-var UserSchema = mongoose.Schema({
-    username: {
+var User = new Schema({
+    email: {
         type: String,
         required: true,
         unique: true
+    },
+    firstname: {
+        type: String,
+        default: ''
+    },
+    birthdate: {
+        type: Date,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
+    },
+    avatar: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Avatar'
+    },
+    parentPin: {
+        type: String,
+        default: '0000'
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    },
+    accountActivated: {
+        type: Boolean,
+        default: false
     }
 });
 
-var User = mongoose.model('User', UserSchema);
+User.plugin(passportLocalMongoose);
 
-module.exports = {
-    User: User
-};
+module.exports = mongoose.model('User', User);
