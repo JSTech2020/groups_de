a760db3488ea8dbf5d8b2ceafb1a1da348f26f13
Ahmad Diaa <ahmaddiaa93@gmail.com>
2020-04-10 19:17:49 +0200

    update project routes to match stories

--- backend-zs/server.js
+++ backend-zs/server.js
@@ -4,26 +4,16 @@ const passport = require('passport');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-<<<<<<< HEAD
-<<<<<<< HEAD
 const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
-=======
-const auth = require("./middleware/auth");
-require('dotenv').config()
 
->>>>>>> 6466538... add jwt authentication middleware and move config to .env files
-=======
 const auth = require("./middleware/auth");
-require('dotenv').config()
 
->>>>>>> 13b15d2... add jwt authentication middleware and move config to .env files
-const app = express();
+const app = express()
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 app.use(cors());
-const router = express.Router();
 
 
 mongoose.connect(process.env.MONGODB, { useNewUrlParser: true });
@@ -39,14 +29,12 @@ app.use(logger(process.env.ENV));
 
 app.listen(process.env.API_PORT, () => console.log(`LISTENING ON PORT ${process.env.API_PORT}`));
 
-const userRoutes = require("./routes/user.routes");
-app.use('/api/', userRoutes(passport));
+
+var userRoutes = require("./routes/user.routes");
+app.use('/api/', auth.authenticateUser, userRoutes());
 
 var storyRoutes = require('./routes/story.routes')
 app.use('/api/stories', auth.authenticateUser, storyRoutes())
 
-app.use('/api/', userRoutes());
-app.use('/api/stories', storyRoutes())
-
 var projectRoutes = require("./routes/project.routes");
-app.use('/api/', projectRoutes());
+app.use('/api/projects', auth.authenticateUser, projectRoutes())
