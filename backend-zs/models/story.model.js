248546958cc83cea6804acb87d77e1a8c1c5e491
Dalia Maarek <daliamaarek@gmail.com>
2020-04-10 19:06:29 +0200

    Story Schema and "GET" endpoints (#25)
    * Added model and a few endpoints (without auth)
    
    * Adjusted schema
    
    * removed delete story endpoint
    
    * select specific features when requesting all stories
    
    * addressed comments
    
    * changed stories' path and adjusted module exports

---
+++ backend-zs/models/story.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+var Story = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    authorImage: {
+        type: String,
+        required: false,
+    },
+    shortDescription: {
+        type: String,
+        required: true
+    },
+    story: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: false,
+    },
+    numberLikes: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'User',
+        default: []
+    },
+    games: [String]
+
+})
+
+module.exports = mongoose.model('Story', Story);
