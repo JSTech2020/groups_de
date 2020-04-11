edb066f7a8385a93f0941b71ea859b32de304e25
Kyle Hiroyasu <kyle.hiroyasu@valiton.com>
2020-04-11 15:32:49 +0200

    basic front end for additional information done

--- frontend-zs/src/App.js
+++ frontend-zs/src/App.js
@@ -6,6 +6,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import EditProfile from "./components/editProfile/EditProfile.component";
 import Header from "./components/header/Header.component";
+import RegistrationStepTwo from './components/registrationStepTwo/RegistrationStepTwo.component';
+
 
 class App extends Component {
   render() {
@@ -13,6 +15,7 @@ class App extends Component {
       <Router>
         <Header></Header>
         <Route path="/editProfile" component={EditProfile} />
+        <Route path="/registrationStepTwo" component={RegistrationStepTwo} />
       </Router>
     );
   }
--- frontend-zs/src/components/registrationStepTwo/RegistrationStepTwo.component.js
+++ frontend-zs/src/components/registrationStepTwo/RegistrationStepTwo.component.js
@@ -1,46 +1,28 @@
 import React from "react";
-//import Form from 'react-bootstrap/Form';
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-//import { yup } from 'yup';
 import * as yup from 'yup';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+//import { Form } from 'formik';
+import { Formik, Field, ErrorMessage } from 'formik';
 
 const schema = yup.object({
-    firstName: yup.string().required(),
-    dateOfBirth: yup.date().max(new Date().toString()).required(),
-    pin: yup.string().required(),
+    firstName: yup.string()
+        .max(15)
+        .matches(/^[a-z]$/i, 'Invalid first name')
+        .required('Required'),
+    dateOfBirth: yup.date()
+        .max(new Date(), 'Invalid Birthdate')
+        .required('Required'),
+    pin: yup.string()
+        .max(4, 'Invalid pin. Must be 4 digits')
+        .matches(/^[0-9]{4}$/, 'Invalid pin. Must be 4 digits')
+        .required('Required'),
 });
 
-// A custom validation function. This must return an object
-// which keys are symmetrical to our values/initialValues
-const validate = values => {
-    const errors = {};
-    if (!values.firstName) {
-        errors.firstName = 'Required';
-    } else if (values.firstName.length > 15) {
-        errors.firstName = 'Must be 15 characters or less';
-    }
-
-    if (!values.dateOfBirth) {
-        errors.dateOfBirth = 'Required';
-    } else if (values.lastName.length > 20) {
-        errors.lastName = 'Must be 20 characters or less';
-    }
-
-    if (!values.pin) {
-        errors.pin = 'Required';
-    } else if (!/^[0-9]{4}$/.test(values.pin)) {
-        errors.pin = 'Invalid pin';
-    }
-
-    return errors;
-};
-
-function RegistrationStepTwo() {
+const RegistrationStepTwo = () => {
     return (
         <Formik
             validationSchema={schema}
-            validate={validate}
             onSubmit={(values, { setSubmitting }) => {
                 setTimeout(() => {
                     alert(JSON.stringify(values, null, 2));
@@ -63,44 +45,22 @@ function RegistrationStepTwo() {
                 errors,
             }) => (
                     <Form noValidate onSubmit={handleSubmit}>
-                        <h3> Account Information</h3>
-                        <Form.Group controlId="formFirstName">
+                        <Form.Group>
+                            <h3> Account Information</h3>
                             <label htmlFor="firstName">First Name</label>
                             <Field name="firstName" type="text" />
                             <ErrorMessage name="firstName" />
                         </Form.Group>
-
-                        <Form.Group controlId="formBirthDate">
-                            <Form.Label>Date of Birth</Form.Label>
-                            <Form.Control
-                                type="date"
-                                id="dateOfBirth"
-                                name="dateOfBirth"
-                                value={values.dateOfBirth}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                isValid={touched.dateOfBirth && !errors.dateOfBirth}
-                            />
-                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                            <Form.Control.Feedback type="invalid">
-                                {errors.dateOfBirth}
-                            </Form.Control.Feedback>
+                        <Form.Group>
+                            <label htmlFor="dateOfBirth">Date of Birth</label>
+                            <Field name="dateOfBirth" type="date" />
+                            <ErrorMessage name="dateOfBirth" />
                         </Form.Group>
-
-                        <Form.Group controlId="validationCustom01">
-                            <Form.Label>Parent Pin</Form.Label>
-                            <Form.Control
-                                id="pin"
-                                name="pin"
-                                type="password"
-                                value={values.pin}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                isValid={touched.pin && !errors.pin}
-                            />
-                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        <Form.Group>
+                            <label htmlFor="pin">Pin</label>
+                            <Field name="pin" type="text" />
+                            <ErrorMessage name="pin" />
                         </Form.Group>
-
                         <Button variant="primary" type="submit">Submit</Button>
                     </Form>
                 )}
@@ -108,7 +68,6 @@ function RegistrationStepTwo() {
 
     );
 
-}
-
+};
 
 export default RegistrationStepTwo;
