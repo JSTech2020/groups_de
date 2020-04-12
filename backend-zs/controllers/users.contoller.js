592a61219e056e62db5cf3e8b8ac0a7bef8ba74b
Jennifer Pousada <>
2020-04-12 22:28:06 +0200

    added updateUser

--- backend-zs/controllers/users.contoller.js
+++ backend-zs/controllers/users.contoller.js
@@ -1,6 +1,7 @@
 const UserModel = require('../models/user.model');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+//const jwtDecode = require(jwt-decode);
 const sgMail = require('@sendgrid/mail');
 
 exports.getUsers = function (req, res) {
@@ -63,3 +64,32 @@ exports.verify = async function(req, res) {
         await res.json({ success: false, message: e })
     }
 };
+
+exports.verifyToken = function(req, res, next) {
+    var token = req.headers['x-access-token'];
+    if (!token)
+      return res.status(403).send({ auth: false, message: 'No token provided.' });
+      
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err)
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        
+      // if everything good, save to request for use in other routes
+      req.userId = decoded.id;
+      next();
+    });
+  };
+  
+
+exports.updateUser = function (req, res, next) {
+   
+
+        User.findById(req.userId, { password: 0 }, function (err, user) {
+          if (err) return res.status(500).send("There was a problem finding the user.");
+          if (!user) return res.status(404).send("No user found.");
+          
+          res.status(200).send(user);
+        });
+        
+      };
+
