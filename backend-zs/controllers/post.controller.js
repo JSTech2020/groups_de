7ad4e523a25c3f61a10482895c1eff9bd6b2ab00
Kyle Hiroyasu <kyle.hiroyasu@valiton.com>
2020-04-19 14:09:18 +0200

    post functionality working, need to add file upload

--- backend-zs/controllers/post.controller.js
+++ backend-zs/controllers/post.controller.js
@@ -1,7 +1,9 @@
 const PostModel = require('../models/posts')
 
 exports.createPost = function (req, res) {
-    PostModel.create(req.body, (err, post) => {
+    var post = req.body;
+    post.user = req.user._id
+    PostModel.create(post, (err, post) => {
         if (err) {
             res.statusCode = 500;
             res.setHeader('Content-Type', 'application/json');
