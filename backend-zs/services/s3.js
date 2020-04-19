f6f66210623cd600f294b62b853d1fb397f8bdac
Ahmad Diaa <ahmaddiaa93@gmail.com>
2020-04-19 04:47:24 +0200

    add endpoint to upload files to s3

--- backend-zs/services/s3.js
+++ backend-zs/services/s3.js
@@ -1,4 +1,7 @@
+var path = require('path')
 var AWS = require('aws-sdk');
+var multer = require('multer')
+var multerS3 = require('multer-s3')
 
 var s3 = new AWS.S3({
     accessKeyId: process.env.S3_ACCESS_KEY_ID,
@@ -7,6 +10,19 @@ var s3 = new AWS.S3({
     s3ForcePathStyle: true,
     signatureVersion: 'v4'
 });
+
+// multer is a middleware for uploading multipart form
+// data to s3
+exports.multerS3 = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.S3_BUCKET_NAME,
+        key: function (req, file, cb) {
+            cb(null, req.user._id + '-' + Date.now() + path.extname(file.originalname))
+        }
+    })
+})
+
 
 exports.deleteElements = function (elements, bucket) {
     elements.forEach(element => {
