6c8d2da0c2dc2a0ceaf495bc8f0db3bdb8437e2f
EdinCitaku <edin.citaku@gmail.com>
2020-04-22 00:05:15 +0200

    dont show privacyPolicy on app start

--- frontend-zs/src/index.js
+++ frontend-zs/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import PrivacyPolicy from "./components/privacyPolicy/PrivacyPolicy";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <PrivacyPolicy />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
