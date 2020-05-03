31c94675dc54470436eb1904bf1fa099ec20e60d
Ahmad Diaa <ahmaddiaa93@gmail.com>
2020-05-03 18:08:43 +0200

    use default image with projects that do not have a project image

--- frontend-zs/src/components/projects/Home/Image.js
+++ frontend-zs/src/components/projects/Home/Image.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import Image from 'react-bootstrap/Image'
-import Gallery from '../SingleProject/Gallery'
+import DefaultImage from '../../../assets/default_image.png'
 
 export function ImageDisplay(imageName, width, height, isCircle, onImageClick) {
     const [image, setImage] = useState("")
@@ -21,12 +21,11 @@ export function ImageDisplay(imageName, width, height, isCircle, onImageClick) {
                     setImage("data:;base64," + base64)
                 });
     }, [imageName])
+    let imageSrc = (!!imageName) ? image : DefaultImage
 
-    return (!!imageName) ? (
-        (isCircle) ?
-            < Image src={image} roundedCircle width={width} height={height} className='mr-md-3' onClick={() => onImageClick()} />
-            : < Image src={image} width={width} height={height} className='mr-md-3' onClick={() => onImageClick()} />
+    return ((isCircle) ?
+        < Image src={imageSrc} roundedCircle width={width} height={height} className='mr-md-3' onClick={() => onImageClick()} />
+        : < Image src={imageSrc} width={width} height={height} className='mr-md-3' onClick={() => onImageClick()} />)
 
-    ) : null
 }
 export default ImageDisplay;
